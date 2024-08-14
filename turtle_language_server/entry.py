c0ad0091163167ea6795c2df367707aaa4b88c41
Jacob <jacob@jaso5.com>
2024-08-14 16:20:15 +0100

    Added --tcp option

--- turtle_language_server/entry.py
+++ turtle_language_server/entry.py
@@ -1,7 +1,14 @@
 from turtle_language_server.server import ttl_server
+from sys import argv
 
 def main():
-    ttl_server.start_io()
+    if argv[1] in ["--tcp"]:
+        ip, port = argv[2].split(':')
+        ttl_server.start_tcp(ip, port)
+    else:
+        ttl_server.start_io()
+
 
 if __name__ == "__main__":
     main()
+
